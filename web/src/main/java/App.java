727b6cea28fa54116938895e71dc38212179f4e0
dyyjiutian <dyyjiutian@126.com>
2019-11-17 18:53:27 +0800

    test devmind

--- web/src/main/java/App.java
+++ web/src/main/java/App.java
@@ -3,6 +3,8 @@ package springDemo;
 public class App {
     public static String getHello() {
         System.out.println("dongyy");
+        
+        System.out.println("test ");
         return "Hello";
     }
 }
